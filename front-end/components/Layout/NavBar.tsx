55eb59dcbf77d096e1588a78f2701799dab50dca
Andy <11356002@ntub.edu.tw>
2026-09-13 02:45:51 +0800

    refactor: make sidebar a navbar
    - remove builtin navbar
    - add navbar component

---
+++ front-end/components/Layout/NavBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { homeContent } from "@/lib/data/home";
+import Link from 'next/link';
+
+import styles from '@/styles/Components/NavBar.module.scss';
+
+export default function SideBar() {  
+  return (
+    <header className={styles.navbar}>
+          <div className={styles.navInner}>
+            <a className={styles.logo} href="/">
+              {homeContent.brand}
+            </a>
+            <nav className={styles.nav}>
+              <Link href="/rent">工具</Link>
+              <Link href="/teach">課程</Link>
+              <Link href="/share">分享</Link>
+            </nav>
+            <button className={styles.loginButton}>登入</button>
+          </div>
+        </header>
+  );
+}
